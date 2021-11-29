6d5a852ee2cb580ea833b6158f8cc57047a2fe39
Shogo <sho5tkmr0802@gmail.com>
2021-11-29 15:47:27 +0900

    Fix: ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-  resources :items, only: %i[index new create]
+  resources :items, only: %i[index new create] do
+    get '/confirm', to: 'items#confirm', on: :collection
+  end
 end
