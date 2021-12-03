058441a46bb68176cc6a5a1848743bedf823b4e1
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-03 22:05:23 +0900

    add: ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
 
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users, only: %i[new create]
   resources :items, only: %i[index new create edit update] do
     get '/confirm', to: 'items#confirm', on: :collection
