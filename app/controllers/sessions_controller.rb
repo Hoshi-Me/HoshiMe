950e4282ccac9eff9e8e3a314d8deb243460b99f
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-04 20:59:16 +0900

    fix: ymlファイル修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     if @user
       redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
