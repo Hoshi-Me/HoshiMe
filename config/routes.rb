7327dfbdf37210ea8d696e5b6087d21e1d95777d
kimurasayo <otaemiiin@gmail.com>
2021-12-05 12:11:09 +0900

    Add: プロフィール編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     get '/confirm', to: 'items#confirm', on: :collection
   end
   resources :calculations, only: %i[index]
-  resource :profiles, only: %i[show]
+  resource :profiles, only: %i[show edit update]
 end
