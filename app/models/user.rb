a1fb12a6c40f1eaee3abd753e891ecc0f40ecbdb
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-11 21:38:16 +0900

    fix: rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.password_confirmation = user.password
-      user.name = "ゲスト"
+      user.name = 'ゲスト'
     end
   end
 end
