cc99b4e2e6a670082bab45819f3242e1c319f23a
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-11 21:03:10 +0900

    add: コントローラ作成

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,2 +1,9 @@
 class GuestSessionsController < ApplicationController
+  skip_before_action :require_login
+
+  def create
+    @user = User.guest
+    auto_login(@user)
+    redirect_to new_board_path(@user)
+  end
 end
