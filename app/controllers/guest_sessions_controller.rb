72c0c36a08104bdf840693dd560a201fbaba0d5d
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-12-12 21:29:07 +0900

    fix: フラッシュメッセージ削除

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -2,6 +2,6 @@ class GuestSessionsController < ApplicationController
   def create
     @user = User.guest
     auto_login(@user)
-    redirect_to new_three_item_path, notice: "ゲスト(#{@user.name})としてログインしました"
+    redirect_to new_three_item_path
   end
 end
