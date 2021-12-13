78d4a782b804c9fe2f7497dd45b0acde1a817c8a
Shogo <sho5tkmr0802@gmail.com>
2021-12-13 23:41:46 +0900

    Fix: ランキングページで３つまとめてレーダーチャート表示

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,6 +1,13 @@
 class CalculationsController < ApplicationController
   def index
     @calculations = Calculation.all.includes(:item).order(sum: :desc)
+    calculations = @calculations.first(3)
+    calculations_name = []
+    calculations.each do |calculation|
+      calculations_name << calculation.item.name
+    end
+    gon.calculations_name = calculations_name
+    gon.calculations = calculations
   end
 
   def show
