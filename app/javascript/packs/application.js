ffb68c60e44441f39b011010afad89863474921f
kimurasayo <otaemiiin@gmail.com>
2021-12-21 21:20:51 +0900

    jRangeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,25 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@fortawesome/fontawesome-free/js/all'
 import "../src/application.scss"
+import "../library/jquery.range.js"
+
 require('jquery')
 require('chart.js')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$('#item_degree').jRange({
+  from: 0,
+  to: 100,
+  step: 10,
+  scale: [0,10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  format: '%s',
+  width: '100%',
+  theme: "theme-blue",
+  showLabels: false,
+  onstatechange: function(){
+    $("#item_degree").trigger('change');
+  }
+});
