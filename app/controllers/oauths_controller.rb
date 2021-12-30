8f698b283527780395a1e12a008a215dc4a10a95
Shogo <sho5tkmr0802@gmail.com>
2021-12-30 10:05:40 +0900

    Fix: twitterログインするとデータを引き継げるように修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,11 +7,11 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if auth_params[:denied].present?
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      redirect_to root_path, success: "#{provider.titleize}でログインしました"
       return
     end
     create_user_from(provider) unless (@user = login_from(provider))
-    redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+    redirect_to root_path, success: "#{provider.titleize}でログインしました"
   end
 
   private
@@ -22,6 +22,11 @@ class OauthsController < ApplicationController
 
   def create_user_from(provider)
     @user = create_from(provider)
+    @user.update(role: 'general')
+    current_user.items.each do |item|
+      item = @user.items.create( name: item.name, degree: item.degree, start_date: item.start_date, finish_date: item.finish_date, price: item.price )
+      item.calculate
+    end
     reset_session
     auto_login(@user)
   end
